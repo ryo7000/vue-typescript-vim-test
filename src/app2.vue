159467dc845de435d18cda9b037be1f31309719f
ryo <ryo7000@gmail.com>
2019-02-14 22:16:06 +0900

    Initial import

---
+++ src/app2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App2 extends Vue {}
+</script>
